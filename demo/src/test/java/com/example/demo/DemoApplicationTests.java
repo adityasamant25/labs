b748f6f225c272315c7d93721f69bf36b689de51
ADSAMANT <adityasamant@gmail.com>
2020-07-18 20:02:53 +0530

    WireMock Lab - Junit 5

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,18 +1,18 @@
 package com.example.demo;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
 import org.assertj.core.api.BDDAssertions;
-import org.junit.Rule;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import io.restassured.RestAssured;
 
@@ -24,52 +24,38 @@ class DemoApplicationTests {
 
 	private static WireMockServer server = new WireMockServer(PORT);
 
-	@Rule
-	public WireMockRule rule = new WireMockRule(8081);
-
 	@Autowired
 	PersonService personService;
 
-	/*
-	 * @Before public void setup() { server.start(); WireMock.configureFor(HOST,
-	 * PORT);
-	 * WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/person/1")).willReturn(
-	 * WireMock.aResponse().withHeader("Content-Type",
-	 * "application/json").withBody("{\"name\":\"m1\"}")));
-	 * WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/person/2")).willReturn(
-	 * WireMock.aResponse().withHeader("Content-Type",
-	 * "application/json").withBody("{\"name\":\"m2\"}"))); }
-	 */
-
-	/*
-	 * @Test public void should_get_two_people() { List<Person> people =
-	 * this.personService.people();
-	 * 
-	 * BDDAssertions.then(people).hasSize(2); }
-	 */
-
-	@Test
-	public void testGetEndPoint() throws URISyntaxException {
+	@BeforeAll
+	public static void setup() {
 		server.start();
 		WireMock.configureFor(HOST, PORT);
 		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/person/1")).willReturn(
 				WireMock.aResponse().withHeader("Content-Type", "application/json").withBody("{\"name\":\"m1\"}")));
 		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/person/2")).willReturn(
 				WireMock.aResponse().withHeader("Content-Type", "application/json").withBody("{\"name\":\"m2\"}")));
+	}
+
+	@Test
+	public void should_get_two_people() {
 		List<Person> people = this.personService.people();
 
 		BDDAssertions.then(people).hasSize(2);
+	}
 
-		RestAssured.given().when().get(new URI("http://localhost:8081/person/1")).then().assertThat().statusCode(200);
+	@Test
+	public void should_get_two_people_using_rest_assured() throws URISyntaxException {
 
+		RestAssured.given().baseUri("http://localhost:8081").when().get("/person/{id}", 1).then().statusCode(200)
+				.contentType("application/json").body("name", Matchers.equalTo("m1"));
+	}
+
+	@AfterAll
+	public static void teardown() {
 		if (null != server && server.isRunning()) {
 			server.shutdown();
 		}
 	}
-
-	/*
-	 * @After public static void teardown() { if (null != server &&
-	 * server.isRunning()) { server.shutdown(); } }
-	 */
 
 }
