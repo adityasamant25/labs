25ebb16164cf40e43f4a89d5bc31c4d4eee85ff4
ADSAMANT <adityasamant@gmail.com>
2020-08-09 20:39:43 +0530

    Added Spring Cloud Contract Stub Runner

--- car-rental/src/test/java/com/example/carrental/CarRentalApplicationTests.java
+++ car-rental/src/test/java/com/example/carrental/CarRentalApplicationTests.java
@@ -2,33 +2,48 @@ package com.example.carrental;
 
 import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureWireMock(port = 9876)
+//@AutoConfigureWireMock(port = 9876)
+@AutoConfigureStubRunner(
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+        ids = "com.example:fraud-detection:+:stubs:9876")
 class CarRentalApplicationTests {
+
+	/*
+	 * @Rule public StubRunnerRule rule = new StubRunnerRule().downloadStub(
+	 * "com.example:fraud-detection:0.0.1-SNAPSHOT:stubs")
+	 * .withPort(9876).stubsMode(StubRunnerProperties.StubsMode.LOCAL);
+	 */
 
 	@Test
 	public void should_return_a_list_of_frauds() {
 		String response = "[\"marcin\",\"josh\"]";
-		WireMock.stubFor(WireMock.get("/fraud123").willReturn(WireMock.aResponse().withBody(response)));
+		/*
+		 * WireMock.stubFor(WireMock.get("/frauds").willReturn(WireMock.aResponse().
+		 * withBody(response)));
+		 */
 
-		String object = new RestTemplate().getForObject("http://localhost:9876/fraud123", String.class);
-
-		BDDAssertions.then(object).isEqualTo(response);
-	}
-	
-	@Test
-	public void should_return_a_list_of_frauds_from_a_real_application() {
-		String response = "[\"marcin\",\"josh\"]";
-
-		String object = new RestTemplate().getForObject("http://localhost:9875/frauds", String.class);
+		String object = new RestTemplate().getForObject("http://localhost:9876/fraud", String.class);
 
 		BDDAssertions.then(object).isEqualTo(response);
 	}
+
+	/*
+	 * @Test public void should_return_a_list_of_frauds_from_a_real_application() {
+	 * String response = "[\"marcin\",\"josh\"]";
+	 * 
+	 * String object = new
+	 * RestTemplate().getForObject("http://localhost:9875/frauds", String.class);
+	 * 
+	 * BDDAssertions.then(object).isEqualTo(response); }
+	 */
 
 }
