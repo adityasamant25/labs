9526249581624858f792e23af6e8aa00cb670c47
ADSAMANT <adityasamant@gmail.com>
2020-08-11 00:38:36 +0530

    Added contract test for messaging

--- car-rental/src/test/java/com/example/carrental/FraudListenerTests.java
+++ car-rental/src/test/java/com/example/carrental/FraudListenerTests.java
@@ -4,24 +4,25 @@ import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.cloud.contract.stubrunner.StubTrigger;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 
 @SpringBootTest
+@AutoConfigureStubRunner(ids = "com.example:fraud-detection", stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 public class FraudListenerTests {
 
 	@Autowired
 	FraudListener fraudListener;
 	
 	@Autowired
-	Sink sink;
+	StubTrigger stubTrigger;
 	
 	@Test
 	public void should_store_a_fraud() {
-		//given a fraud
-		Fraud fraud = new Fraud("m");
+		
 		//when a message is sent
-		this.sink.input().send(MessageBuilder.withPayload(fraud).build());
+		this.stubTrigger.trigger("trigger_fraud");
 		
 		//then we store a fraud
 		
