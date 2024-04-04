5b26e35a0b7a88a69928294b18f9142d42652986
Aditya Samant <adityasamant.learnings@gmail.com>
2024-04-04 11:21:14 +0530

    Istio scenario trial

--- istio/01-installation/step1/verify.sh
+++ istio/01-installation/step1/verify.sh
@@ -4,11 +4,11 @@
 
 binary_name="istioctl"
 
-if command -v "$binary_name" &> /dev/null
+if command -v "$binary_name" &> /dev/null;
 then
-    echo "$binary_name is installed."
-    exit 0
+    echo "$binary_name is installed.";
+    exit 0;
 else
-    echo "$binary_name is not installed."
-    exit 1
+    echo "$binary_name is not installed.";
+    exit 1;
 fi
