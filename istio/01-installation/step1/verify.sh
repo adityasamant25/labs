8e759060434d3f50f77d43a5050627ce24099db3
Aditya Samant <adityasamant.learnings@gmail.com>
2024-04-04 13:27:44 +0530

    Istio scenario validation

--- istio/01-installation/step1/verify.sh
+++ istio/01-installation/step1/verify.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-output=$(istioctl version)
+#output=$(istioctl version)
 
-if [[ $output == 1* ]]; then
-  echo "istioctl is installed"
+#if [[ $output == 1* ]]; then
+#  echo "istioctl is installed"
+#  exit 0
+#else
+#  echo "istioctl is not installed"
+#  exit 1
+#fi
+
+if istioctl version; then
   exit 0
 else
-  echo "istioctl is not installed"
   exit 1
 fi
